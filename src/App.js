6f1b7bf440b71554dc795af1e9adddbea1a1cb90
Nico López <nnicolopez@gmail.com>
2019-02-04 01:37:13 -0300

    Create component for layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+
 import './App.scss';
+
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <Layout>
+        <p>Test</p>
+      </Layout>
     );
   }
 }
