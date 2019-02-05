8ce1cb19f44b3f167b440a0d280a96e01e34a350
Nico López <nlopez@tribpub.com>
2019-02-05 10:49:03 -0300

    Add basic burger structure

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
+import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
     render () {
         return (
             <>
-                <div>Burger</div>
+                <Burger />
                 <div>Build controls</div>
             </>
         );
