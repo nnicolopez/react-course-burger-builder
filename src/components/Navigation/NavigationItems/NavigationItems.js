d4eb2ce2d915653e6785bc66041510d8ddc3e0db
Nico López <nnicolopez@gmail.com>
2019-02-12 23:24:27 -0300

    Add logo and navigation

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './NavigationItems.module.scss';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const navigationItems = (props) => (
+    <ul className={classes.NavigationItems}>
+        <NavigationItem active>Burger Builder</NavigationItem>
+        <NavigationItem>Checkout</NavigationItem>
+
+    </ul>
+);
+
+export default navigationItems;
