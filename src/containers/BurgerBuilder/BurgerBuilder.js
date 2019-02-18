1ed3f877bb268c488e1666d5d7ee8b83359a40af
Nico López <nnicolopez@gmail.com>
2019-02-18 08:47:30 -0300

    Store orders in a DB

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -45,7 +46,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('Continue');
+        // alert('Continue');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'fake name',
+                address: {
+                    street: 'fake street',
+                    zipCode: '01011',
+                    country: 'neverland'
+                }
+            },
+            email: 'test@test.com',
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then( response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     addIngredientHandler = (type) => {
@@ -92,7 +110,7 @@ class BurgerBuilder extends Component {
                 <Modal
                     show={this.state.purchasing}
                     modalClose={this.purchaseCancelHandler}>
-                    <OrderSummary 
+                    <OrderSummary
                         continue={this.purchaseContinueHandler}
                         cancel={this.purchaseCancelHandler}
                         ingredients={this.state.ingredients}
