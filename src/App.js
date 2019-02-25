e57646a6d4c87a83611bf87a92b111a092307e03
Nico López <nlopez@tribpub.com>
2019-02-25 18:00:33 -0300

    Add a checkout page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.scss';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   render() {
     return (
       <Layout>
         <BurgerBuilder />
+        <Checkout />
       </Layout>
     );
   }
