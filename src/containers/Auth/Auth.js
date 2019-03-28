f79f8268412ecd8194aac6857994cd610e70cb36
Nico López <nlopez@tribpub.com>
2019-03-28 17:26:40 -0300

    Redirect to checkout when login while building the burger

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -16,6 +16,12 @@ class Auth extends Component {
         isSignup: true
     }
 
+    componentDidMount () {
+        if (!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
+            this.props.onSetAuthRedirectPath();
+        }
+    }
+
     formFieldInit (tag, type, placeholder, value, validationRules) {
         return {
             elementType: tag,
@@ -108,7 +114,7 @@ class Auth extends Component {
 
         let authRedirect = null;
         if (this.props.isAunthenticated) {
-            authRedirect = <Redirect to="/" />
+            authRedirect = <Redirect to={this.props.authRedirectPath} />
         }
 
         return (
@@ -129,13 +135,16 @@ const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
         error: state.auth.error,
-        isAunthenticated: state.auth.token !== null
+        isAunthenticated: state.auth.token !== null,
+        buildingBurger: state.burgerBuilder.building,
+        authRedirectPath: state.auth.authRedirectPath
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
+        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
+        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
     }
 }
 
